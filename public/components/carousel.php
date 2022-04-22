95650edbe742592981e7f5ffb198c9bf5f083294
Rammah <rammahkarpous@outlook.com>
2022-04-22 09:03:48 +0200

    moved all the items function from carousel.php to fake-data.php

--- public/components/carousel.php
+++ public/components/carousel.php
@@ -1,69 +1,8 @@
 <?php
-function items() {
-    return [
-        [ 
-            "image" => [ 
-                "url" => "image.png", 
-                "alt" => "Haal meer uit Facebook met deze 7 tips voor gratis engagement" 
-            ], 
-            "title" => "Haal meer uit Facebook met deze 7 tips voor gratis engagement" 
-        ],
-        [ 
-            "image" => [ 
-                "url" => "image.png", 
-                "alt" => "8 tips voor meer engagement op Facebook [infographic]" 
-            ], 
-            "title" => "8 tips voor meer engagement op Facebook [infographic]" 
-        ],
-        [ 
-            "image" => [ 
-                "url" => "image.png", 
-                "alt" => "Facebook & organisch bereik: wat je vooral níet moet doen" 
-            ], 
-            "title" => "Facebook & organisch bereik: wat je vooral níet moet doen" 
-        ],
-        [ 
-            "image" => [ 
-                "url" => "image.png", 
-                "alt" => "Adverteren of sponsored posts op Facebook, waar kies je voor?" 
-            ], 
-            "title" => "Adverteren of sponsored posts op Facebook, waar kies je voor?" 
-        ],
-        [ 
-            "image" => [ 
-                "url" => "image.png", 
-                "alt" => "-- Haal meer uit Facebook met deze 7 tips voor gratis engagement 2" 
-            ], 
-            "title" => "-- Haal meer uit Facebook met deze 7 tips voor gratis engagement 2" 
-        ],
-        [ 
-            "image" => [ 
-                "url" => "image.png", 
-                "alt" => "-- 8 tips voor meer engagement op Facebook [infographic] 2" 
-            ], 
-            "title" => "-- 8 tips voor meer engagement op Facebook [infographic] 2" 
-        ],
-        [ 
-            "image" => [ 
-                "url" => "image.png", 
-                "alt" => "-- Facebook & organisch bereik: wat je vooral níet moet doen 2" 
-            ], 
-            "title" => "-- Facebook & organisch bereik: wat je vooral níet moet doen 2" 
-        ],
-        [ 
-            "image" => [ 
-                "url" => "image.png", 
-                "alt" => "-- Adverteren of sponsored posts op Facebook, waar kies je voor? 2" 
-            ], 
-            "title" => "-- Adverteren of sponsored posts op Facebook, waar kies je voor? 2" 
-        ],
-    ];
-}
+
+include( 'data/fake-posts.php' );
 
 $title = "Facebook voor beginners";
-
-function image( $item ) { echo items()[ 0 ][ $item ]; }
-function text( $item ) { echo items()[ $item ]; }
 ?>
 
 <div class="carousel">
@@ -75,8 +14,8 @@ function text( $item ) { echo items()[ $item ]; }
         <div class="carousel__slides">
             <?php foreach(items() as $item) : ?>
                 <div class="slide">
-                    <!-- <img src="<?php // echo $item[ 'image' ][ 'url' ] ?>" alt="<?php // echo $item[ 'image' ][ 'alt' ] ?>"> -->
-                    <span class="slide__image"></span>
+                    <img src="<?php echo $item[ 'image' ][ 'url' ] ?>" alt="<?php echo $item[ 'image' ][ 'alt' ] ?>">
+                    <!-- <span class="slide__image"></span> -->
                     <h4 class="slide__title"><?php echo $item[ 'title' ] ?></h4>
                 </div>
             <?php endforeach;?>
---
+++ public/data/fake-posts.php
@@ -0,0 +1,62 @@
+<?php
+function items() {
+    return [
+        [ 
+            "image" => [ 
+                "url" => "../assets/image.png", 
+                "alt" => "Haal meer uit Facebook met deze 7 tips voor gratis engagement" 
+            ], 
+            "title" => "Haal meer uit Facebook met deze 7 tips voor gratis engagement" 
+        ],
+        [ 
+            "image" => [ 
+                "url" => "../assets/image.png", 
+                "alt" => "8 tips voor meer engagement op Facebook [infographic]" 
+            ], 
+            "title" => "8 tips voor meer engagement op Facebook [infographic]" 
+        ],
+        [ 
+            "image" => [ 
+                "url" => "../assets/image.png", 
+                "alt" => "Facebook & organisch bereik: wat je vooral níet moet doen" 
+            ], 
+            "title" => "Facebook & organisch bereik: wat je vooral níet moet doen" 
+        ],
+        [ 
+            "image" => [ 
+                "url" => "../assets/image.png", 
+                "alt" => "Adverteren of sponsored posts op Facebook, waar kies je voor?" 
+            ], 
+            "title" => "Adverteren of sponsored posts op Facebook, waar kies je voor?" 
+        ],
+        [ 
+            "image" => [ 
+                "url" => "../assets/image.png", 
+                "alt" => "-- Haal meer uit Facebook met deze 7 tips voor gratis engagement 2" 
+            ], 
+            "title" => "-- Haal meer uit Facebook met deze 7 tips voor gratis engagement 2" 
+        ],
+        [ 
+            "image" => [ 
+                "url" => "../assets/image.png", 
+                "alt" => "-- 8 tips voor meer engagement op Facebook [infographic] 2" 
+            ], 
+            "title" => "-- 8 tips voor meer engagement op Facebook [infographic] 2" 
+        ],
+        [ 
+            "image" => [ 
+                "url" => "../assets/image.png", 
+                "alt" => "-- Facebook & organisch bereik: wat je vooral níet moet doen 2" 
+            ], 
+            "title" => "-- Facebook & organisch bereik: wat je vooral níet moet doen 2" 
+        ],
+        [ 
+            "image" => [ 
+                "url" => "../assets/image.png", 
+                "alt" => "-- Adverteren of sponsored posts op Facebook, waar kies je voor? 2" 
+            ], 
+            "title" => "-- Adverteren of sponsored posts op Facebook, waar kies je voor? 2" 
+        ],
+    ];
+}
+?>
